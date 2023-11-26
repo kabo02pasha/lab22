49d3b73efd0ad38ec0f8ff447f840b0cd7749b05
pasha143 <jypashkaler89328121@gmail.com>
2023-11-26 14:09:54 +0300

    Initial commit

---
+++ src/ru/mirea/lab22/Number2v1/CalculatorApp.java
@@ -0,0 +1,9 @@
+package ru.mirea.lab22.Number2v1;
+public class CalculatorApp {
+    public static void main(String[] args) {
+        CalculatorView view = new CalculatorView();
+        CalculatorModel model = new CalculatorModel();
+        CalculatorController controller = new CalculatorController(view, model);
+        view.setVisible(true);
+    }
+}
---
+++ src/ru/mirea/lab22/Number2v1/CalculatorModel.java
@@ -0,0 +1,6 @@
+package ru.mirea.lab22.Number2v1;
+public class CalculatorModel {
+    public String calculate(String expression) {
+        return expression;
+    }
+}
